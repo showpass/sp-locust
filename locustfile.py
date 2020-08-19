0d0d240e8858ad11dbe0432fad67362269fdb7e0
Peter P <peter@showpass.com>
2020-08-18 21:15:56 -0600

    fix | locust file

--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,17 @@
-from locust import HttpLocust
+from locust import HttpUser
 
 import settings
 from tasks.front import PerformanceBasket, FrontPage
 
 if settings.PERF_ENABLED:
-    class PerformanceUser(HttpLocust):
+    class PerformanceUser(HttpUser):
         task_set = PerformanceBasket
         min_wait = 500
         max_wait = 1000
 
 
 if settings.FRONT_PAGE_ENABLED:
-    class FrontPageUser(HttpLocust):
+    class FrontPageUser(HttpUser):
         task_set = FrontPage
         min_wait = 500
         max_wait = 1000
